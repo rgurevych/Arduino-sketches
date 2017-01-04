f3a4b9a2d5ce7620001935648b6d9fd909a2d42c
Rostyslav Gurevych <rostikg@gmail.com>
2017-01-04 18:35:15 +0100

    First sketch - blinking LED

---
+++ Blinking_LED/Blinking_LED.ino
@@ -0,0 +1,17 @@
+//This is my first program
+//It should blink with a LED
+
+int ledPin = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(ledPin, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  delay(500);
+}

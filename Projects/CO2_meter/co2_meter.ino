69ecaafe59c531de4ab655789b1e2c9bf052dd6a
Rostyslav Gurevych <rostyslav.gurevych@idnow.de>
2020-06-09 21:06:32 +0300

    Refactor the functions, add daily data array

--- Projects/CO2_meter/co2_meter.ino
+++ Projects/CO2_meter/co2_meter.ino
@@ -11,10 +11,18 @@
 #define TX_PIN 9   //Serial tx pin no
 
 
+// Timer durations
+#define MEASURE_TIMEOUT 15000
+#define PRINT_TIMEOUT 30000
+#define WARMING_UP_TIMEOUT 175000
+
+
 // Timers:
-GTimer measureTimer(MS, 30000);
-GTimer printTimer(MS, 15000);
+GTimer measureTimer(MS, MEASURE_TIMEOUT);
+GTimer printTimer(MS, PRINT_TIMEOUT);
+GTimer fiveMinTimer(MS, 300000);
 GTimer hourlyTimer(MS, 3600000);
+GTimer warmingTimer(MS);
 
 
 // CO2 sensor:
@@ -26,21 +34,32 @@ DHT dht(DHTPIN, DHT22);
 
 
 // Initial variables:
-long t = 0;
+long t = PRINT_TIMEOUT/1000;
 int current_ppm = 0;
-int max_ppm = 0;
-int min_ppm = 5000;
+int max_5min_ppm = 0;
+int min_5min_ppm = 5000;
+int max_hour_ppm = 0;
+int min_hour_ppm = 5000;
 float current_temp = 0;
 float current_hum = 0;
 
+int minHourPpmArray[12], maxHourPpmArray[12];
+int minDayPpmArray[24], maxDayPpmArray[24];
+
+
+// Flags
+boolean warmedUpFlag = false;
 
 void setup() {
   // Serial
   Serial.begin(9600);
 
+  // Start warming up timer
+  warmingTimer.setTimeout(WARMING_UP_TIMEOUT);
+
   // MH-Z19
   mhz19.begin(RX_PIN, TX_PIN);
-  //mhz19.setAutoCalibration(false);  //  uncomment this to disable autocalibration
+  mhz19.setAutoCalibration(false);  //  uncomment this to disable autocalibration
   mhz19.getStatus();    // first request, should return -1
   delay(500);
   if (mhz19.getStatus() == 0) {
@@ -62,26 +81,11 @@ void setup() {
 
 
 void loop(){
-  if (measureTimer.isReady()){
-    current_ppm = measureCO2();
-    current_temp = measureTemp();
-    current_hum = measureHum();
-    
-    if (current_ppm > max_ppm) max_ppm = current_ppm;
-    if (current_ppm < min_ppm) min_ppm = current_ppm;
-  }
-
-  if (printTimer.isReady()){
-    Serial.print(String(t)); Serial.print(" - CO2: "); Serial.print(current_ppm); Serial.print(" ppm\t");
-    Serial.print("Humidity: "); Serial.print(current_hum); Serial.print(" %\t"); Serial.print("Temperature: "); Serial.print(current_temp); Serial.println(" *C.");
-    t += 15;
-  }
-
-  if (hourlyTimer.isReady()){
-    Serial.print("Max value for last hour: "); Serial.print(max_ppm); Serial.print("; Min value for last hour:"); Serial.println(min_ppm);
-    max_ppm = 0;
-    min_ppm = 5000;
-  }
+  
+  measure();
+  printSerialCurrentValues();
+  updateData();
+  
 }
 
 
@@ -100,4 +104,126 @@ float measureTemp(){
 float measureHum(){
   float hum = dht.readHumidity();
   return hum;
+}
+
+
+void measure(){
+  if (warmingTimer.isReady()){
+    warmedUpFlag = true;
+  }
+  
+  if (measureTimer.isReady()){
+    current_ppm = measureCO2();
+    current_temp = measureTemp();
+    current_hum = measureHum();
+  }
+}
+
+
+void updateData(){
+  if (warmedUpFlag){
+    if (current_ppm > max_5min_ppm) max_5min_ppm = current_ppm;
+    if (current_ppm < min_5min_ppm) min_5min_ppm = current_ppm;
+  }
+  
+  if (fiveMinTimer.isReady()){
+    Serial.print(F("Max value for last 5 minutes: ")); Serial.print(max_5min_ppm); Serial.print(F("; Min value for last 5 minutes:")); Serial.println(min_5min_ppm);
+
+    for (byte i = 0; i < 11; i++) {
+      minHourPpmArray[i] = minHourPpmArray[i + 1];
+      maxHourPpmArray[i] = maxHourPpmArray[i + 1];
+    }
+    minHourPpmArray[11] = min_5min_ppm;
+    maxHourPpmArray[11] = max_5min_ppm;
+
+    Serial.print(F("maxHourPpmArray = "));
+    for (byte i = 0; i < 12; i++) {
+      Serial.print(maxHourPpmArray[i]);
+      Serial.print(F("; "));
+    }
+    Serial.println(F(""));
+
+    Serial.print(F("minHourPpmArray = "));
+    for (byte i = 0; i < 12; i++) {
+      Serial.print(minHourPpmArray[i]);
+      Serial.print(F("; "));
+    }
+    Serial.println(F(""));
+     
+    max_5min_ppm = 0;
+    min_5min_ppm = 5000;
+  }
+
+    if (hourlyTimer.isReady()){
+
+    max_hour_ppm = calculateMaxFromArray(maxHourPpmArray, 12);
+    min_hour_ppm = calculateMinFromArray(minHourPpmArray, 12);
+    
+    Serial.print(F("Max value for last hour: ")); Serial.print(max_hour_ppm); Serial.print(F("; Min value for last hour:")); Serial.println(min_hour_ppm);
+
+    for (byte i = 0; i < 23; i++) {
+      minDayPpmArray[i] = minDayPpmArray[i + 1];
+      maxDayPpmArray[i] = maxDayPpmArray[i + 1];
+    }
+    minDayPpmArray[23] = min_hour_ppm;
+    maxDayPpmArray[23] = max_hour_ppm;
+
+    Serial.print(F("maxDayPpmArray = "));
+    for (byte i = 0; i < 24; i++) {
+      Serial.print(maxDayPpmArray[i]);
+      Serial.print(F("; "));
+    }
+    Serial.println(F(""));
+
+    Serial.print(F("minDayPpmArray = "));
+    for (byte i = 0; i < 24; i++) {
+      Serial.print(minDayPpmArray[i]);
+      Serial.print(F("; "));
+    }
+    Serial.println(F(""));
+  }
+}
+
+
+int calculateMaxFromArray(int *dataArray, byte arrSize) {
+  int max_value = 0;
+  for (byte i = 0; i < arrSize; i++) {
+    if (dataArray[i] > max_value){
+      max_value = dataArray[i];
+    }
+  }
+
+  return max_value;
+}
+
+
+int calculateMinFromArray(int *dataArray, byte arrSize) {
+  int min_value = 5000;
+  for (byte i = 0; i < arrSize; i++) {
+    if (dataArray[i] < min_value && dataArray[i] != 0){
+      min_value = dataArray[i];
+    }
+  }
+
+  return min_value;
+}
+
+
+void printSerialCurrentValues(){  
+  if (printTimer.isReady()){
+    Serial.print(String(t)); 
+    Serial.print(F(" - CO2: "));
+    if (!warmedUpFlag){
+      Serial.print(F("**"));
+    }
+    Serial.print(current_ppm); 
+    Serial.print(F(" ppm\t"));
+    Serial.print(F("Humidity: ")); 
+    Serial.print(current_hum); 
+    Serial.print(F(" %\t")); 
+    Serial.print(F("Temperature: ")); 
+    Serial.print(current_temp); 
+    Serial.println(F(" *C."));
+    t += PRINT_TIMEOUT/1000; 
+  }
 }

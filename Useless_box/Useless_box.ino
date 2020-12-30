728f397f986653c337f5167604b6fdd0d37910a0
Rostyslav Gurevych <rostyslav.gurevych@idnow.de>
2020-12-30 19:53:31 +0200

    Working version with first mode_0

--- Useless_box/Useless_box.ino
+++ Useless_box/Useless_box.ino
@@ -8,6 +8,10 @@
 
 
 // Settings
+#define MAX_HAND_SERVO 20                //Max allowed angle of hand servo
+#define MAX_BOX_SERVO 150
+#define DEFAULT_HAND_SERVO_SPEED 180
+#define DEFAULT_BOX_SERVO_SPEED 100
 
 
 // Pins
@@ -20,7 +24,8 @@
 
 
 // Timers
-GTimer delayTimer(MS);
+GTimer delayTimer(MS);                //Delay timer used in each particular step
+GTimer switchDelayTimer(MS, 1000);    //Timer used between enabling switch and starting operation
 
 
 // Objects
@@ -32,7 +37,7 @@ ServoSmooth boxServo;
 boolean led_flag, hand_servo_state, box_servo_state;
 boolean operate_flag = false;
 byte operation_step = 0;
-byte max_hand_servo = 20;
+byte mode = 0;
 
 uint32_t myTimer;
 
@@ -46,13 +51,13 @@ void setup() {
   handServo.smoothStart();
   handServo.setTargetDeg(180);
   handServo.setAccel(0);
-  handServo.setSpeed(180);
+  handServo.setSpeed(DEFAULT_HAND_SERVO_SPEED);
 
   boxServo.attach(SERVO_BOX_PIN, 180);
   boxServo.smoothStart();
   boxServo.setTargetDeg(180);
   boxServo.setAccel(0);
-  boxServo.setSpeed(100);
+  boxServo.setSpeed(DEFAULT_BOX_SERVO_SPEED);
 }
 
 
@@ -67,6 +72,7 @@ void loop() {
 void check_switch(){
   if (digitalRead(SWITCH_PIN) == HIGH && !operate_flag){
     operate_flag = true;
+    switchDelayTimer.start();
   }
   if (operate_flag) digitalWrite(LED_PIN, HIGH);
   else digitalWrite(LED_PIN, LOW);
@@ -75,17 +81,62 @@ void check_switch(){
 
 void operate(){
   if (operate_flag){
-    if (operation_step == 0){
+    if (mode == 0) mode_0();
+    else if (mode == 1) mode_1();
+  }
+}
+
+
+void mode_0(){
+    if (operation_step == 0 && switchDelayTimer.isReady()){
+      operation_step ++;
+      delayTimer.setTimeout(2500);
+      boxServo.setTargetDeg(MAX_BOX_SERVO);
+      boxServo.tick();
+    }
+
+    if (operation_step == 1 && delayTimer.isReady()){
+      operation_step ++;
+      handServo.setSpeed(120);
+      delayTimer.setTimeout(2000);
+      handServo.setTargetDeg(MAX_HAND_SERVO);
+      handServo.tick();
+    }
+
+    if (operation_step == 2 && delayTimer.isReady()){
+      operation_step ++;
+      delayTimer.setTimeout(2000);
+      handServo.setTargetDeg(180);
+      handServo.tick();
+    }
+
+    if (operation_step == 3 && delayTimer.isReady()){
+      operation_step ++;
+      delayTimer.setTimeout(800);
+      boxServo.setTargetDeg(180);
+      boxServo.tick();
+    }
+    
+    if (operation_step == 4 && delayTimer.isReady()){
+      handServo.setSpeed(DEFAULT_HAND_SERVO_SPEED);
+      operation_step = 0;
+      operate_flag = false;
+    }
+}
+
+
+void mode_1(){
+    if (operation_step == 0 && switchDelayTimer.isReady()){
       operation_step ++;
       delayTimer.setTimeout(1500);
-      boxServo.setTargetDeg(150);
+      boxServo.setTargetDeg(MAX_BOX_SERVO);
       boxServo.tick();
     }
 
     if (operation_step == 1 && delayTimer.isReady()){
       operation_step ++;
       delayTimer.start();
-      handServo.setTargetDeg(max_hand_servo);
+      handServo.setTargetDeg(MAX_HAND_SERVO);
       handServo.tick();
     }
 
@@ -107,5 +158,4 @@ void operate(){
       operation_step = 0;
       operate_flag = false;
     }
-  }
 }

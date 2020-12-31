5bf5f455c8960207fa0840b10c1dc10090fd13a3
Rostyslav Gurevych <rostyslav.gurevych@idnow.de>
2020-12-31 17:02:41 +0200

    Add mode 8

--- Useless_box/Useless_box.ino
+++ Useless_box/Useless_box.ino
@@ -37,7 +37,7 @@ ServoSmooth boxServo;
 boolean led_flag, hand_servo_state, box_servo_state;
 boolean operate_flag = false;
 byte operation_step = 0;
-byte mode = 7;
+byte mode = 8;
 
 uint32_t myTimer;
 
@@ -89,6 +89,7 @@ void operate(){
     else if (mode == 5) mode_5();
     else if (mode == 6) mode_6();
     else if (mode == 7) mode_7();
+    else if (mode == 8) mode_8();
   }
 }
 
@@ -529,6 +530,86 @@ void mode_7(){
       operation_step = 0;
       operate_flag = false;
       handServo.setAccel(0);
+    }
+}
+
+
+void mode_8(){
+    if (operation_step == 0 && switchDelayTimer.isReady()){
+      operation_step ++;
+      delayTimer.setTimeout(500);
+      boxServo.setTargetDeg(MAX_BOX_SERVO);
+      boxServo.tick();
+    }
+
+    if (operation_step == 1 && delayTimer.isReady()){
+      operation_step ++;
+      handServo.setAccel(0.5);
+      delayTimer.setTimeout(2000);
+      handServo.setTargetDeg(MAX_HAND_SERVO);
+      handServo.tick();
+    }
+
+    if (operation_step == 2 && delayTimer.isReady()){
+      operation_step ++;
+      handServo.setAccel(0);
+      delayTimer.setTimeout(500);
+      handServo.setTargetDeg(MAX_HAND_SERVO + 30);
+      handServo.tick();
+    }
+
+    if (operation_step == 3 && delayTimer.isReady()){
+      operation_step ++;
+      delayTimer.start();
+      handServo.setTargetDeg(MAX_HAND_SERVO);
+      handServo.tick();
+    }
+
+    if (operation_step == 4 && delayTimer.isReady()){
+      operation_step ++;
+      delayTimer.start();
+      handServo.setTargetDeg(MAX_HAND_SERVO + 30);
+      handServo.tick();
+    }
+
+    if (operation_step == 5 && delayTimer.isReady()){
+      operation_step ++;
+      delayTimer.start();
+      handServo.setTargetDeg(MAX_HAND_SERVO);
+      handServo.tick();
+    }
+
+    if (operation_step == 6 && delayTimer.isReady()){
+      operation_step ++;
+      delayTimer.start();
+      handServo.setTargetDeg(MAX_HAND_SERVO + 30);
+      handServo.tick();
+    }
+    
+    if (operation_step == 7 && delayTimer.isReady()){
+      operation_step ++;
+      delayTimer.start();
+      handServo.setTargetDeg(MAX_HAND_SERVO);
+      handServo.tick();
+    }
+
+    if (operation_step == 8 && delayTimer.isReady()){
+      operation_step ++;
+      delayTimer.setTimeout(800);
+      handServo.setTargetDeg(180);
+      handServo.tick();
+    }
+
+    if (operation_step == 9 && delayTimer.isReady()){
+      operation_step ++;
+      delayTimer.setTimeout(500);
+      boxServo.setTargetDeg(180);
+      boxServo.tick();
+    }
+    
+    if (operation_step == 10 && delayTimer.isReady()){
+      operation_step = 0;
+      operate_flag = false;
     }
 }
 

e233182ac4abb013e66b7dd749ddce16cfe4fd4e
Rostyslav Gurevych <rostyslav.gurevych@idnow.de>
2021-12-11 13:19:29 +0200

    Add RTC module and encoder

--- Power_meter/Power_meter.ino
+++ Power_meter/Power_meter.ino
@@ -6,20 +6,27 @@
 #include <GyverTimer.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <RTClib.h>
+#include <GyverEncoder.h>
 
 
 // Pins
 #define PZEM_RX_PIN 12
 #define PZEM_TX_PIN 13
+#define CLKe 5        // encoder S1
+#define DTe 4         // encoder S2
+#define SWe 3        // encoder Key
 
 
 // Timer durations
-#define MEASURE_TIMEOUT 2000             //Interval between measure occurs
-#define PRINT_TIMEOUT 1000               //Interval between printing to the screen occurs
+#define MEASURE_TIMEOUT 1500             //Interval between measure occurs
+#define PRINT_TIMEOUT 500               //Interval between printing to the screen occurs
 
 
 // Settings
-bool DEBUG_MODE = true;
+#define DEBUG_MODE 1
+#define RESET_CLOCK 0
+#define ENCODER_TYPE 1
 bool DEMO_MODE = true;
 
 
@@ -32,30 +39,47 @@ GTimer printTimer(MS, PRINT_TIMEOUT);
 SoftwareSerial pzemSWSerial(PZEM_TX_PIN, PZEM_RX_PIN);
 PZEM004Tv30 pzem;
 LiquidCrystal_I2C lcd(0x27, 20, 4);
+RTC_DS3231 rtc;
+DateTime now;
+Encoder enc(CLKe, DTe, SWe);
 
 
 // Global variables
-int mom_voltage = 0;
-int mom_current = 0;
-int mom_power = 0;
+uint8_t hour, min, second, month, day;
+uint16_t year;
+uint16_t mom_voltage = 0;
+uint16_t mom_current = 0;
+uint16_t mom_power = 0;
 long mom_energy = 0;
-int mom_frequency = 0;
-int mom_pf = 0;
-byte mode = 1;
+uint16_t mom_frequency = 0;
+uint16_t mom_pf = 0;
+byte mode = 0;
+byte screen = 0;
 
 //Flags
 bool screenReadyFlag = false;
+bool lcdBacklight = true;
 
 
 void setup() {
   if (DEBUG_MODE) {
     Serial.begin(115200);
   }
+
+  // RTC module
+  rtc.begin();
+  if (RESET_CLOCK || rtc.lostPower()){
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+  now = rtc.now();
   
   pzem = PZEM004Tv30(pzemSWSerial);
+  
+  enc.setType(ENCODER_TYPE);
+  
   // analogWrite(BACKLIGHT, LCD_BRIGHTNESS);
   lcd.init();
-  lcd.backlight();
+  lcd.setBacklight(lcdBacklight);
   lcd.clear();
   lcd.setCursor(4, 0);
   lcd.print(F("POWER METER"));
@@ -71,6 +95,7 @@ void setup() {
     lcd.setCursor(2, 1);
     lcd.print(F("Demo mode enabled"));
     randomSeed(analogRead(0));
+    screen = 1;
   }
   else {
     if (pzem.readAddress() == 0) {
@@ -95,7 +120,10 @@ void setup() {
 
 
 void loop() {
+  enc.tick();
   getPowerData();
+  checkMode();
+  checkBacklight();
 }
 
 
@@ -107,7 +135,6 @@ void getPowerData(){
     else {
       measurePower();
     }
-  printPowerData();
   }
 }
 
@@ -117,8 +144,8 @@ void measurePower(){
     if (DEBUG_MODE) {
       Serial.println(F("The meter is not powered!"));
     }
-    if (mode == 1) {
-      mode = 0;
+    if (mode == 0 && screen == 1) {
+      screen = 0;
       screenReadyFlag = false;
     }    
     mom_voltage = 0;
@@ -129,8 +156,8 @@ void measurePower(){
   }
   else {
     readPowerData();
-    if (mode == 0) {
-      mode = 1;
+    if (mode == 0 && screen == 0) {
+      screen = 1;
       screenReadyFlag = false;
     }
   }
@@ -154,60 +181,92 @@ void generatePowerData(){
   mom_voltage = 2100 + random(200);
   mom_current = random(1500);
   mom_power = round((mom_voltage / 10.0) * (mom_current / 100.0));
-  mom_energy += mom_power / 360;
+  mom_energy += mom_power / 1800;
   mom_frequency = random(498, 502);
   mom_pf = random(90, 101);
-  
+}
+
+
+void checkMode(){
+  if (mode == 0) {printPowerData();}
 }
 
 void printPowerData() {
-  if (DEBUG_MODE) {  
-    Serial.print(F("U: "));      Serial.print(mom_voltage / 10.0);      Serial.println(F("V"));
-    Serial.print(F("I: "));      Serial.print(mom_current / 100.0);     Serial.println(F("A"));
-    Serial.print(F("P: "));      Serial.print(mom_power);               Serial.println(F("W"));
-    Serial.print(F("E: "));      Serial.print(mom_energy / 10.0);       Serial.println(F("kWh"));
-    Serial.print(F("F: "));      Serial.print(mom_frequency / 100.0);   Serial.println(F("Hz"));
-    Serial.print(F("PF: "));     Serial.println(mom_pf / 100.0);        Serial.println();
-  }
+  if (printTimer.isReady()){
 
-  if (!screenReadyFlag) {
-    lcd.clear();
-  }
+    now = rtc.now();
+    second = now.second();
+    min = now.minute();
+    hour = now.hour();
+    day = now.day();
+    month = now.month();
+    year = now.year();
+    
+    if (DEBUG_MODE) {
+      Serial.print(hour); Serial.print(F(":")); Serial.print(min); Serial.print(F(":")); Serial.print(second);
+      Serial.print(F("  ")); Serial.print(day); Serial.print(F("/")); Serial.print(month); Serial.print(F("/")); Serial.println(year);
+      Serial.print(F("U: "));      Serial.print(mom_voltage / 10.0);      Serial.println(F("V"));
+      Serial.print(F("I: "));      Serial.print(mom_current / 100.0);     Serial.println(F("A"));
+      Serial.print(F("P: "));      Serial.print(mom_power);               Serial.println(F("W"));
+      Serial.print(F("E: "));      Serial.print(mom_energy / 10.0);       Serial.println(F("kWh"));
+      Serial.print(F("F: "));      Serial.print(mom_frequency / 10.0);    Serial.println(F("Hz"));
+      Serial.print(F("PF: "));     Serial.println(mom_pf / 100.0);        Serial.println();
+    }
+
+    if (!screenReadyFlag) {
+      lcd.clear();
+    }
+    
+    if (screen == 0) {
+      if (!screenReadyFlag){
+        lcd.setCursor(5, 0);
+        lcd.print(F("The meter"));
+        lcd.setCursor(3, 1);
+        lcd.print(F("is not powered"));
+        screenReadyFlag = true;
+      }
+    }
   
-  if (mode == 0) {
-    if (!screenReadyFlag){
-      lcd.setCursor(5, 0);
-      lcd.print(F("The meter"));
-      lcd.setCursor(3, 1);
-      lcd.print(F("is not powered"));
-      screenReadyFlag = true;
+    if (screen == 1) {
+      if (!screenReadyFlag){
+        if (DEMO_MODE) {
+          lcd.setCursor(18, 0); lcd.print(F("DM"));
+        }
+        else {
+          lcd.setCursor(18, 0); lcd.print(F("NW"));
+        }
+        lcd.setCursor(2, 0); lcd.print(F(":")); lcd.setCursor(5,0); lcd.print(F(":"));
+        lcd.setCursor(11, 0); lcd.print(F("/")); lcd.setCursor(14,0); lcd.print(F("/"));
+        lcd.setCursor(0, 1); lcd.print(F("U=")); lcd.setCursor(7,1); lcd.print(F("V"));
+        lcd.setCursor(12, 1); lcd.print(F("I=")); lcd.setCursor(19,1); lcd.print(F("A"));
+        lcd.setCursor(0, 2); lcd.print(F("P=")); lcd.setCursor(7,2); lcd.print(F("W"));
+        lcd.setCursor(12, 2); lcd.print(F("F=")); lcd.setCursor(18,2); lcd.print(F("Hz"));
+        lcd.setCursor(0, 3); lcd.print(F("E=")); lcd.setCursor(8,3); lcd.print(F("kWh"));
+        lcd.setCursor(13, 3); lcd.print(F("PF=")); 
+        screenReadyFlag = true;
+      }
+  
+      lcd.setCursor(0,0); if (hour < 10){lcd.print(F("0"));} lcd.print(hour); 
+      lcd.setCursor(3,0); if (min < 10){lcd.print(F("0"));} lcd.print(min); 
+      lcd.setCursor(6,0); if (second < 10){lcd.print(F("0"));} lcd.print(second);
+      lcd.setCursor(9,0); if (day < 10){lcd.print(F("0"));} lcd.print(day); 
+      lcd.setCursor(12,0); if (month < 10){lcd.print(F("0"));} lcd.print(month); 
+      lcd.setCursor(15,0); lcd.print(year-2000);
+      lcd.setCursor(2,1); lcd.print(mom_voltage / 10.0, 1);
+      lcd.setCursor(14,1); if(mom_current < 1000){lcd.print(F(" "));} lcd.print(mom_current / 100.0, 2);
+      lcd.setCursor(2,2); if(mom_power < 10000){lcd.print(F(" "));} if(mom_power < 1000){lcd.print(F(" "));} 
+      if(mom_power < 100){lcd.print(F(" "));} if(mom_power < 10){lcd.print(F(" "));} lcd.print(mom_power);
+      lcd.setCursor(14,2); lcd.print(mom_frequency / 10.0, 1);
+      lcd.setCursor(2,3); if(mom_energy < 10000){lcd.print(F(" "));} if(mom_energy < 1000){lcd.print(F(" "));} 
+      if(mom_energy < 100){lcd.print(F(" "));} lcd.print(mom_energy / 10.0, 1);
+      lcd.setCursor(16,3); lcd.print(mom_pf / 100.0);
     }
   }
+}
 
-  if (mode == 1) {
-    if (!screenReadyFlag){
-      if (DEMO_MODE) {
-        lcd.setCursor(5, 0); lcd.print(F("Demo mode:"));
-      }
-      else {
-        lcd.setCursor(3, 0); lcd.print(F("Current status:"));
-      }
-      lcd.setCursor(0, 1); lcd.print(F("U=")); lcd.setCursor(7,1); lcd.print(F("V"));
-      lcd.setCursor(12, 1); lcd.print(F("I=")); lcd.setCursor(19,1); lcd.print(F("A"));
-      lcd.setCursor(0, 2); lcd.print(F("P=")); lcd.setCursor(7,2); lcd.print(F("W"));
-      lcd.setCursor(12, 2); lcd.print(F("F=")); lcd.setCursor(18,2); lcd.print(F("Hz"));
-      lcd.setCursor(0, 3); lcd.print(F("E=")); lcd.setCursor(8,3); lcd.print(F("kWh"));
-      lcd.setCursor(13, 3); lcd.print(F("PF=")); 
-      screenReadyFlag = true;
-    }
-
-    lcd.setCursor(2,1); lcd.print(mom_voltage / 10.0, 1);
-    lcd.setCursor(14,1); if(mom_current < 1000){lcd.print(F(" "));} lcd.print(mom_current / 100.0, 2);
-    lcd.setCursor(2,2); if(mom_power < 10000){lcd.print(F(" "));} if(mom_power < 1000){lcd.print(F(" "));} 
-    if(mom_power < 100){lcd.print(F(" "));} if(mom_power < 10){lcd.print(F(" "));} lcd.print(mom_power);
-    lcd.setCursor(14,2); lcd.print(mom_frequency / 10.0, 1);
-    lcd.setCursor(2,3); if(mom_energy < 10000){lcd.print(F(" "));} if(mom_energy < 1000){lcd.print(F(" "));} 
-    if(mom_energy < 100){lcd.print(F(" "));} lcd.print(mom_energy / 10.0, 1);
-    lcd.setCursor(16,3); lcd.print(mom_pf / 100.0);
+void checkBacklight(){
+  if(enc.isClick()) {
+    lcdBacklight = !lcdBacklight;
+    lcd.setBacklight(lcdBacklight);
   }
 }

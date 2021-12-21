bdf5aee5d1559b18a8e7b89ad1df971d9ee8f011
Rostyslav Gurevych <rostyslav.gurevych@idnow.de>
2021-12-21 15:08:04 +0200

    Separate meter.ino file (all methods related to meter itself). Add Meter setup menu

--- Power_meter/Power_meter.ino
+++ Power_meter/Power_meter.ino
@@ -158,66 +158,6 @@ void loop() {
 }
 
 
-void getPowerData(){
-  if (measureTimer.isReady()){
-    if (DEMO_MODE){
-      generatePowerData();
-    }
-    else {
-      measurePower();
-    }
-  }
-}
-
-
-void measurePower(){  
-  if (pzem.readAddress() == 0){
-    if (DEBUG_MODE) {
-      Serial.println(F("The meter is not powered!"));
-    }
-    if (mode == 0 && screen == 1) {
-      screen = 0;
-      screenReadyFlag = false;
-    }    
-    mom_voltage = 0;
-    mom_current = 0;
-    mom_power = 0;
-    mom_frequency = 0;
-    mom_pf = 0;
-  }
-  else {
-    readPowerData();
-    if (mode == 0 && screen == 0) {
-      screen = 1;
-      screenReadyFlag = false;
-    }
-  }
-}
-
-
-void readPowerData() {
-  mom_voltage = pzem.voltage() * 10;
-  mom_current = round(pzem.current() * 100);
-  mom_power = round(pzem.power());
-  mom_energy = round(pzem.energy() * 10);
-  mom_frequency = pzem.frequency() * 10;
-  mom_pf = pzem.pf() * 100;
-  if (isnan(mom_voltage)) {
-    mom_voltage = 0;
-  }
-}
-
-
-void generatePowerData(){
-  mom_voltage = 2100 + random(200);
-  mom_current = random(1500);
-  mom_power = round((mom_voltage / 10.0) * (mom_current / 100.0));
-  mom_energy += mom_power / 1800;
-  mom_frequency = random(498, 502);
-  mom_pf = random(90, 101);
-}
-
-
 void checkMode(){
   if(mode != 0 && menuExitTimer.isReady()) {
     mode = 0;
@@ -357,54 +297,6 @@ void backgroundTime(){
 }
 
 
-void recordMeter(){
-  if(min == 0 && !recordMeterDoneFlag){
-    if (pzem.readAddress() != 0 || DEMO_MODE) {
-      updateMeter();
-    }
-    recordMeterDoneFlag = true;
-  }
-  else if(min != 0) {
-    recordMeterDoneFlag = false;
-  }
-}
-
-
-void updateMeter(){
-  byte s;
-  float recordEnergy;
-  if (DEMO_MODE) {
-    s = 100;
-    recordEnergy = mom_energy / 10.0;
-  }
-  else {
-    s = 0;
-    recordEnergy = pzem.energy();
-  }
-  
-  EEPROM.get(0+s, latest_energy);
-  EEPROM.get(4+s, day_energy);
-  EEPROM.get(8+s, night_energy);
-  EEPROM.get(12+s, total_energy);
-  float energyDelta = recordEnergy - latest_energy;           //Считаем разницу между текущими и сохраненными ранее показаниями
-  if (energyDelta < 0){                                       //Если разница меньше 0, значит счетчик сбрасывали
-    energyDelta = recordEnergy;                               //поэтому в таком случае учитываем полное значение счетчика (разница между текущими показаниями и 0)
-  }
-  EEPROM.put(0+s, recordEnergy);
-
-  if(hour > DAY_TARIFF_START && hour <= NIGHT_TARIFF_START) {
-    day_energy += energyDelta;
-    EEPROM.put(4+s, day_energy);
-  }
-  else {
-    night_energy += energyDelta;
-    EEPROM.put(8+s, night_energy);
-  }
-  total_energy = day_energy + night_energy;
-  EEPROM.put(12+s, total_energy);
-}
-
-
 void mainMenu(){
   if (!screenReadyFlag) {
     lcd.clear();
@@ -536,36 +428,6 @@ void settingsMenu(){
 //    }
     enc.resetState();
     menuExitTimer.start();
-  }
-}
-
-
-void showMeter(){
-  byte s;
-  if (DEMO_MODE) s = 100; 
-  else s = 0;
-  
-  if (!screenReadyFlag) {
-    lcd.clear();
-    }
-    
-  if (!screenReadyFlag){
-    EEPROM.get(4+s, day_energy);
-    EEPROM.get(8+s, night_energy);
-    EEPROM.get(12+s, total_energy);
-    
-    lcd.setCursor(4, 0);  lcd.print(F("Energy meter"));
-    lcd.setCursor(0, 1);  lcd.print(F("Day:"));  printEnergy(day_energy, true);  lcd.print(F("kWh"));
-    lcd.setCursor(0, 2);  lcd.print(F("Ngt:"));  printEnergy(night_energy, true);  lcd.print(F("kWh"));
-    lcd.setCursor(0, 3);  lcd.print(F("Tot:"));  printEnergy(total_energy, true);  lcd.print(F("kWh"));
-    screenReadyFlag = true;
-    }
-  
-  if(enc.click()){
-    mode = 1;
-    screenReadyFlag = false;
-
-    enc.resetState();
   }
 }
 
@@ -713,8 +575,7 @@ void setMeter(){
     lcd.setCursor(0, 2);  lcd.print(F("Ngt:")); lcd.setCursor(12, 2); lcd.print(F("kWh"));
     lcd.setCursor(2, 3);  lcd.print(F("Back"));
     lcd.setCursor(12, 3);  lcd.print(F("Save"));
-    EEPROM.get(4+s, day_energy);
-    EEPROM.get(8+s, night_energy);
+    getMeterData(s);
     screenReadyFlag = true;
     }
 
